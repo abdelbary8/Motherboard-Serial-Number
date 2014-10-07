75f06d36c035a7e43b563c3081ac5736bd74a10c
abdelbary8 <abdelbary8@yahoo.com>
2014-10-08 00:29:48 +0530

    First Build

---
+++ Motherboard/Motherboard.cs
@@ -0,0 +1,31 @@
+﻿using System.Management;
+
+namespace Motherboard
+{
+    public class Board
+    {
+        ManagementObjectSearcher managementObjectSearcher;
+
+        public string SerialNumber()
+        {
+            managementObjectSearcher = new ManagementObjectSearcher((ObjectQuery)new SelectQuery("Win32_BaseBoard"));
+            ManagementObjectCollection.ManagementObjectEnumerator enumerator = null;
+            string str = "";
+
+            try
+            {
+                enumerator = managementObjectSearcher.Get().GetEnumerator();
+                while (enumerator.MoveNext())
+                {
+                    str = enumerator.Current["SerialNumber"].ToString();
+                }
+            }
+            finally
+            {
+                if (enumerator != null) enumerator.Dispose();
+            }
+            return str;
+        }
+
+    }
+}
---
+++ MotherboardTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using Motherboard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotherboardTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board mb = new Board();
+            Console.WriteLine(mb.SerialNumber());
+            Console.ReadKey();
+
+        }
+    }
+}
